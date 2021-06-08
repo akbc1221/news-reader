c640bf35a55624720d4ad41626be17adbbd8c23d
Arnab Chattopadhyay <59999832+akbc1221@users.noreply.github.com>
2021-06-08 13:22:37 +0530

    added proxy cors

--- src/App.js
+++ src/App.js
@@ -13,8 +13,9 @@ const App = () => {
 
   const newsApi = async () => {
     try {
+      const proxyUrl = "https://cors-anywhere.herokuapp.com/";
       const news = await axios.get(
-        `https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.REACT_APP_API_KEY}&pageSize=${loadMore}&category=${category}`
+        `${proxyUrl}https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.REACT_APP_API_KEY}&pageSize=${loadMore}&category=${category}`
       );
       setNewsArray(news.data.articles);
       setNewsResults(news.data.totalResults);
